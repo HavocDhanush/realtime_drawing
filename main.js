70923b3f488b57cc959e80a135b28960c56529ef
HavocDhanush <93704624+HavocDhanush@users.noreply.github.com>
2023-11-13 15:32:05 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -11,6 +18,11 @@ function setup() {
 
 function draw() {
     background ('#969A97');
+
+    document.getElementById("square_side").innerHTML = "Width and Height of a square will be = " + difference + "px";
+    fill('#F90093');
+    stroke('F90093');
+    square(noseX, noseY, difference);
 }
 
 function modelLoaded() {
@@ -21,6 +33,15 @@ function gotPoses (results) {
     if (results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = "+ noseX +" noseY = "+ noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference);
     }
 }
 
